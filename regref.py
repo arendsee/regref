2f9bc33b75b858f92b36c68127996c401667a7d4
Zebulun Arendsee <arendsee@iastate.edu>
2014-09-11 17:18:54 -0500

    release 2.0 (NOT backwards compatible)

--- regref.py
+++ regref.py
@@ -1,31 +1,16 @@
 #!/usr/bin/env python3
 
+
 import argparse
 import re
 import sys
+import collections
 
-__version__ = '1.1'
+__version__ = '2.0'
 __prog__ = 'regref'
 
 def parser(argv=None):
-    parser = argparse.ArgumentParser(
-        prog = __prog__,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog = \
-        '''
-        Example:
-        $ cat pat.tab
-         asdf 1234
-         qwer 2345
-        $ cat in.tab
-         file1 ID=asdf
-         file2 ID=qwer
-         file4 ID=rrrr
-        $ regref -f pat.tab -m 'ID=(\S+)' -p 'ID=(\S+)' -r 'ID={1}' < in.tab
-         file1 ID=1234
-         file2 ID=2345
-        '''
-    )
+    parser = argparse.ArgumentParser()
     parser.add_argument(
         '--version',
         help='Display version',
@@ -33,108 +18,168 @@ def parser(argv=None):
         version='%(prog)s {}'.format(__version__)
     )
     parser.add_argument(
-        '-f', '--pattern-file',
-        metavar='file',
-        help='Space delimited file containing columns of regex patterns'
+        'map',
+        metavar='MAP',
+        type=argparse.FileType('r'),
+        help='space delimited file containing columns of strings'
     )
     parser.add_argument(
-        '-m', '--matcher',
-        metavar='str',
-        help='Matching pattern'
+        'pat',
+        metavar='PAT',
+        nargs="?",
+        help="regex pattern, '${n}' references nth column from MAP"
     )
     parser.add_argument(
-        '-p', '--pattern',
-        metavar='str',
-        help='Regex pattern'
+        'rep',
+        metavar='REP',
+        default=None,
+        nargs="?",
+        help="replacement pattern, '${n}' references nth column from MAP"
     )
     parser.add_argument(
-        '-r', '--replacement',
-        metavar='str',
-        help='Replacement regex pattern'
-    )
-    parser.add_argument(
-        '-a', '--write-all',
-        help='Write all lines, even if they do not match',
-        action='store_true',
-        default=False
-    )
-    parser.add_argument(
-        '-v', '--invert',
-        help='Return only non-matching (does nothing if -m and -p)',
-        action='store_true',
-        default=False
+        '--where',
+        nargs=2,
+        metavar='COND',
+        help='ARG1: a regex with one capture. ARG2: column number for capture'
     )
     args = parser.parse_args(argv)
+
+    if args.where:
+        if len(args.where) < 2:
+            sys.exit("--where must have at least two arguments (e.g. --where 'id=(\d+)' 3)")
+        else:
+            Anchor = collections.namedtuple('Anchor', ['capture', 'column'])
+            args.where = Anchor(args.where[0], int(args.where[1]))
+
     return(args)
 
-class Reg:
-    def __init__(self, args):
-        self._pat = self._load_patterns(args)
-        self._match = self._compile_pattern(args.matcher)
-        self._ppat = args.pattern
-        self._rpat = args.replacement
 
-    def _load_patterns(self, args):
-        raise NotImplemented
+class Selector:
+    def __init__(self, capture, column, mapfile):
+        self.capture = re.compile(capture)
+        self.column = column
+        self.repmap = self._load_map(mapfile)
 
-    def _compile_pattern(self, pat):
+    def _load_map(self, mapfile):
+        repmap = dict()
+        for line in mapfile:
+            row = line.split()
+            try:
+                if row[self.column] in repmap:
+                    msg = 'Column {} of MAP is not a unique key'
+                    sys.exit(msg.format(self.column))
+                repmap[row[self.column]] = row
+            except IndexError:
+                msg = 'MAP must have at least {} columns ({} found)'
+                sys.exit(msg.format(self.column, len(row)))
+        return(repmap)
+
+    def get_maprow(self, line):
         try:
-            cpat = re.compile(pat)
-        except:
-            sys.exit('Invalid patter "%s"' % pat)
-        return(cpat)
+            m = re.search(self.capture, line)
+            rowid = m.group(1)
+            row = self.repmap[rowid]
+        except (KeyError, IndexError, AttributeError):
+            row = None
+        return(row)
 
-    def _get_key(self, line):
+class Regref:
+    def __init__(self, args, data=sys.stdin):
+
+        opt = 4*bool(args.where) + 2*bool(args.pat) + bool(args.rep)
+
+        self.pat = args.pat
+        self.rep = args.rep
+        self.data = data
+
+        if opt in (4, 6, 7):
+            self.selector = Selector(args.where.capture, args.where.column, args.map)
+        else:
+            self.patmap = [s.split() for s in args.map]
+
+        # delete O(mn)
+        if opt == 2:
+            self.gen = self.delete_mn
+
+        # search and replace O(mn)
+        elif opt == 3:
+            self.gen = self.search_and_replace_mn
+
+        # search O(m + n)
+        elif opt == 4:
+            self.gen = self.search_m_plus_n
+
+        # delete O(m + n)
+        elif opt == 6:
+            self.gen = self.delete_m_plus_n
+
+        # search and replace O(m + n)
+        elif opt == 7:
+            self.gen = self.search_and_replace_m_plus_n
+
+        else:
+            sys.exit('Invalid option combination')
+
+    def _replace(self, base_pat, base_rep, row, line):
+        pat = self._specify_regex(base_pat, row)
+        rep = self._specify_regex(base_rep, row)
         try:
-            return(re.search(self._match, line).group(1))
-        except:
-            return(None)
-
-    def read(self, line):
-        raise NotImplemented
-
-class Formatter(Reg):
-    def _load_patterns(self, args):
-        with open(args.pattern_file, 'r') as f:
-            pat = {c[0]:c[1:] for c in (r.split() for r in f.readlines())}
-        return(pat)
-
-    def read(self, line):
-        key = self._get_key(line)
-        try:
-            var = [key] + self._pat[key]
-        except:
-            return(None)
-        out = re.sub(self._ppat.format(*var),
-                     self._rpat.format(*var),
-                     line)
+            out = re.sub(pat, rep, line)
+        except re.error as e:
+            sys.exit('Invalid regular expression: {}'.format(e))
         return(out)
 
-class Searcher(Reg):
-    def _load_patterns(self, args):
-        with open(args.pattern_file, 'r') as f:
-            pat = {c.strip() for c in f.readlines()}
-        return(pat)
+    def _delete(self, base_pat, row, line):
+        pat = self._specify_regex(base_pat, row)
+        try:
+            out = re.sub(pat, '', line)
+        except re.error as e:
+            sys.exit('Invalid regular expression: {}'.format(e))
+        return(out)
 
-    def read(self, line):
-        key = self._get_key(line)
-        return(key in self._pat)
+    def _specify_regex(self, r, row):
+        try:
+            rout = re.sub('\$\{(\d+)\}', lambda m: row[int(m.groups(1)[0])], r)
+        except IndexError:
+            sys.exit('MAP lacks column requested column (index starts at 0)')
+        except re.error:
+            sys.exit("Invalid regular expression: '{}'".format(r))
+        return(rout)
+
+    def delete_mn(self):
+        for line in self.data:
+            for row in self.patmap:
+                line = self._delete(self.pat, row, line)
+            yield line
+
+    def search_and_replace_mn(self):
+        for line in self.data:
+            for row in self.patmap:
+                line = self._replace(self.pat, self.rep, row, line)
+            yield line
+
+    def search_m_plus_n(self):
+        for line in self.data:
+            if self.selector.get_maprow(line):
+                yield line
+
+    def delete_m_plus_n(self):
+        for line in self.data:
+            row = self.selector.get_maprow(line)
+            if row:
+                line = self._delete(self.pat, row, line)
+            yield line
+
+    def search_and_replace_m_plus_n(self):
+        for line in self.data:
+            row = self.selector.get_maprow(line)
+            if row:
+                line = self._replace(self.pat, self.rep, row, line)
+            yield line
+
 
 if __name__ == '__main__':
     args = parser()
-    if(args.pattern and args.replacement):
-        reformatter = Formatter(args)
-        for line in sys.stdin:
-            line = line.strip()
-            out = reformatter.read(line)
-            if out:
-                print(out)
-            elif args.write_all:
-                print(line)
-    else:
-        searcher = Searcher(args)
-        for line in sys.stdin:
-            line = line.strip()
-            matched = searcher.read(line)
-            if (matched and not args.invert) or (not matched and args.invert):
-                print(line)
+
+    for line in Regref(args).gen():
+        print(line, end='')
